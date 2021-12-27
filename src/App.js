836c2c20a321744500506d3320f8c87946a097fd
Gaurav <gauravpawar3102@gmail.com>
2021-12-27 18:43:13 +0530

    Api added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Row from './Row';
+import Row from './Row.jsx';
 import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-      />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchTopRated} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTopRated} />
     </div>
   );
 }
